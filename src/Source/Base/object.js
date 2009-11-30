891616a1ed7872bec9efcf85e2e8c03f1133cc1f
pagameba <pagameba@0cc02af8-7455-0410-a6ac-531894676941>
2009-11-30 23:04:46 +0000

    initial documentation for Jx.Object.

--- src/Source/Base/object.js
+++ src/Source/Base/object.js
@@ -5,6 +5,78 @@
  * implements both mootools mixins Events and Options so the rest of the
  * classes don't need to.
  *
+ * The Initialization Pipeline:
+ * Jx.Object provides a default initialize method to construct new instances
+ * of objects that inherit from it.  No sub-class should override initialize
+ * unless you know exactly what you doing.  Instead, the initialization
+ * pipeline provides an init() method that is intended to be overridden in
+ * sub-classes to provide class-specific initialization as part of the
+ * initialization pipeline.
+ *
+ * The basic initialization pipeline for a Jx.Object is to parse the
+ * parameters provided to initialize(), separate out options from other formal
+ * parameters based on the parameters property of the class, call init() and
+ * initialize plugins.  
+ *
+ * Parsing Parameters:
+ * Because each sub-class no longer has an initialize method, it no longer has
+ * direct access to parameters passed to the constructor.  Instead, a 
+ * sub-class is expected to provide a parameters attribute with an array of
+ * parameter names in the order expected.  Jx.Object will enumerate the 
+ * attributes passed to its initialize method and automatically place them
+ * in the options object under the appropriate key (the value from the 
+ * array).  Parameters not found will not be present or will be null.
+ *
+ * The default parameters are a single options object which is merged with
+ * the options attribute of the class.
+ *
+ * Calling Init:
+ * Jx.Object fires the event 'preInit' before calling the init() method,
+ * calls the init() method, then fires the 'postInit' event.  It is expected
+ * that most sub-class specific initialization will happen in the init()
+ * method.  A sub-class may hook preInit and postInit events to perform tasks
+ * by overriding the initialize method as follows:
+ *
+ * (code)
+ * var MyClass = new Class({
+ *   Family: 'MyClass',
+ *   initialize: function() {
+ *     this.addEvent('preInit', this.preInit.bind(this));
+ *     this.addEvent('postInit', this.postInit.bind(this));
+ *     this.parent.apply(this, arguments);
+ *   },
+ *   preInit: function() {
+ *     // something just before init() is called
+ *   },
+ *   postInit: function() {
+ *     // something just after init() is called
+ *   },
+ *   init: function() {
+ *     this.parent();
+ *     // initialization code here
+ *   }
+ * });
+ * (end)
+ *
+ * Plugins:
+ *
+ * Destroying Jx.Object Instances:
+ * Jx.Object provides a destroy method that cleans up potential memory leaks
+ * when you no longer need an object.  Sub-classes are expected to implement
+ * a cleanup() method that provides specific cleanup code for each
+ * sub-class.  Remember to call this.parent() when providing a cleanup()
+ * method.
+ *
+ * The Family Attribute:
+ * the Family attribute of a class is used internally by JxLib to identify Jx
+ * objects within mootools.  The actual value of Family is unimportant to Jx.
+ * If you do not provide a Family, a class will inherit it's base class family
+ * up to Jx.Object.  Family is useful when debugging as you will be able to
+ * identify the family in the firebug inspector, but is not as useful for
+ * coding purposes as it does not allow for inheritance.
+ *
+ * Parameters:
+ * 
  * Example:
  * (code)
  * (end)
