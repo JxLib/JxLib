a183c4686d74bc5959ab30607407230338e01d08
conrad.barthelmes <conrad.barthelmes@0cc02af8-7455-0410-a6ac-531894676941>
2010-05-07 19:25:03 +0000

    forgot to add the other russian file

---
+++ src/Source/locales/locale.russian.unicode.js
@@ -0,0 +1,74 @@
+MooTools.lang.set('ru-RU-unicode', 'Jx', {
+	
+	'widget': {
+		busyMessage: 'Обработка...'
+	},
+	'colorpalette': {
+		alphaLabel: 'alpha (%)'
+	},
+	notice: {
+		closeTip: 'закрыть это сообщение'
+	},
+	progressbar: {
+		messageText: 'Загрузка...',
+		progressText: '{progress} из {total}'
+	},
+	field: {
+		requiredText: '*'
+	},
+	file: {
+		browseLabel: 'Выбрать...'
+	},
+	'formatter.boolean': {
+		'true': 'Да',
+		'false': 'Нет'
+	},
+	'formatter.currency': {
+		sign: 'р.'
+	},
+	'formatter.number': {
+		decimalSeparator: ',',
+    thousandsSeparator: ' '
+	},
+	splitter: {
+		barToolTip: 'потяни, чтобы изменить размер'
+	},
+	panelset: {
+		barToolTip: 'потяни, чтобы изменить размер'
+	},
+	panel: {
+		collapseTooltip: 'Свернуть/Развернуть Панель',
+    collapseLabel: 'Свернуть',
+    expandLabel: 'Развернуть',
+    maximizeTooltip: 'Увеличить Панель',
+    maximizeLabel: 'Увеличить',
+    restoreTooltip: 'Восстановить Панель',
+    restoreLabel: 'Восстановить',
+    closeTooltip: 'Закрыть Панель',
+    closeLabel: 'Закрыть'
+	},
+	confirm: {
+		affirmativeLabel: 'Да',
+    negativeLabel: 'Нет'
+	},
+	dialog: {
+		resizeToolTip: 'Изменить размер'
+	},
+	message: {
+		okButton: 'Ок'
+	},
+	prompt: {
+		okButton: 'Ок',
+		cancelButton: 'Отмена'
+	},
+	upload: {
+		buttonText: 'Загрузка файла'
+	},
+	'plugin.resize': {
+	  tooltip: 'Потяни, чтобы изменить, двойной щелчок для авто размера.'
+	},
+  'plugin.editor': {
+    submitButton: 'Сохранить',
+    cancelButton: 'Отмена'
+  }
+});
