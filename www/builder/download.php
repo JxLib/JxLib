ba2244f6f007b3a5c2600022cc6a28169ce355ef
jonlb@comcast.net <jonlb@comcast.net@0cc02af8-7455-0410-a6ac-531894676941>
2010-06-01 23:53:07 +0000

    changes to Builder to accomodate issue 125. The downloaded file now contains custom built css files (both compressed and uncompressed) as only the images for the components chosen.

--- www/builder/download.php
+++ www/builder/download.php
@@ -9,23 +9,36 @@ define('DS',DIRECTORY_SEPARATOR);
 
 $file = $_REQUEST['file'];
 
-$sections = explode(DS,$file);
+//echo "File = $file";
 
+$sections = explode('/',$file);
+
+$archiveName = 'work'.DS.$file;
 
 //make sure the file we're getting is in the work directory
-if (count($sections) != 2 && !is_dir('work'.DS.$sections[0])){
-	die('No valid file todownload...');
+if (count($sections) != 2 && !file_exists('work'.DS.$sections[0])){
+	die('No valid file to download...');
 }
 
 $archiveName = 'work'.DS.$file;
 
 //send the file to the browser
+
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=".$sections[1]);
 header("Content-length: ".@filesize($archiveName));
 readfile($archiveName);
 
-unlink($archiveName);
-rmdir($work_dir.$sections[0]);
 
-?>
+//remove all working files from the server
+$iterator = new RecursiveDirectoryIterator(dirname(__FILE__).DS.'work'.DS.$sections[0]);
+foreach (new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $filename => $file) {
+    if ($file->isFile()) {
+         //echo "<br>checking $filename";
+        unlink($file->getRealPath());
+    } else {
+        rmdir($file->getRealPath());
+    }
+}
+
+rmdir(dirname(__FILE__).DS.'work'.DS.$sections[0]);
