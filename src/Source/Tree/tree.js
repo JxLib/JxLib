2368e80fc7526fba7355ba1da217b8415d041843
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-17 00:15:36 -0700

    trying to make virtual columns work

--- src/Source/Tree/tree.js
+++ src/Source/Tree/tree.js
@@ -179,7 +179,7 @@ Jx.Tree = new Class({
             return;
         }
         item.addEvents({
-            //add: function(what) { this.fireEvent('add', what).bind(this); },
+            add: function(what) { this.fireEvent('add', what).bind(this); },
             remove: function(what) { this.fireEvent('remove', what).bind(this); },
             disclose: function(what) { this.fireEvent('disclose', what).bind(this); }
         });
@@ -188,7 +188,7 @@ Jx.Tree = new Class({
         this.list.add(item, position);
         this.setDirty(true);
         this.update(true);
-        this.fireEvent('add',item);
+        //this.fireEvent('add',item);
         return this;
     },
     /**
