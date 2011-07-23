69e2953606b8149ef3571456dba52ffd47638bf1
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-23 16:24:17 -0700

    revamping the tree classes

--- src/Source/Tree/tree.old.js
+++ src/Source/Tree/tree.old.js
@@ -38,6 +38,7 @@ images:
  *
  * This file is licensed under an MIT style license
  */
+ /*
 Jx.Tree = new Class({
     
     Extends: Jx.Widget,
@@ -47,7 +48,7 @@ Jx.Tree = new Class({
     /**
      * APIProperty: selection
      * {<Jx.Selection>} the selection object for this tree.
-     */
+     *
     selection: null,
     /**
      * Property: ownsSelection
@@ -55,32 +56,32 @@ Jx.Tree = new Class({
      * or not.  If true then the selection object will be destroyed when the
      * tree is destroyed, otherwise the selection object will not be
      * destroyed.
-     */
+     *
     ownsSelection: false,
     /**
      * Property: list
      * {<Jx.List>} the list object is used to manage the DOM elements of the
      * items added to the tree.
-     */
+     *
     list: null,
     dirty: true,
     /**
      * APIProperty: domObj
      * {HTMLElement} the DOM element that contains the visual representation
      * of the tree.
-     */
+     *
     domObj: null,
     options: {
         /**
          * Option: select
          * {Boolean} are items in the tree selectable?  See <Jx.Selection>
          * for other options relating to selections that can be set here.
-         */
+         *
         select: true,
         /**
          * Option: template
          * the default HTML template for a tree can be overridden
-         */
+         *
         template: '<ul class="jxTreeRoot"></ul>'
     },
     /**
@@ -88,7 +89,7 @@ Jx.Tree = new Class({
      * {Object} anobject of property to CSS class names for extracting references
      * to DOM elements from the supplied templates.  Requires
      * domObj element, anything else is optional.
-     */
+     *
     classes: {domObj: 'jxTreeRoot'},
     
     frozen: false,
@@ -96,7 +97,7 @@ Jx.Tree = new Class({
     /**
      * APIMethod: render
      * Render the Jx.Tree.
-     */
+     *
     render: function() {
         this.parent();
         if (this.options.container !== undefined &&
@@ -146,12 +147,12 @@ Jx.Tree = new Class({
      * the tree will still display the changes but it will delay potentially
      * expensive recursion across the entire tree on every change just to
      * update visual styles.
-     */
+     *
     freeze: function() { this.frozen = true; },
     /**
      * APIMethod: thaw
      * unfreeze the tree and recursively update styles
-     */
+     *
     thaw: function() { this.frozen = false; this.update(true); },
     
     setDirty: function(state) {
@@ -173,7 +174,7 @@ Jx.Tree = new Class({
      *
      * Returns:
      * {<Jx.Tree>} a reference to this object for chaining calls
-     */
+     *
     add: function(item, position) {
         if (Jx.type(item) == 'array') {
             item.each(function(what){ this.add(what, position); }.bind(this) );
@@ -200,7 +201,7 @@ Jx.Tree = new Class({
      *
      * Returns:
      * {<Jx.Tree>} a reference to this object for chaining calls
-     */
+     *
     remove: function(item) {
         item.removeEvents('add');
         item.removeEvents('remove');
@@ -223,7 +224,7 @@ Jx.Tree = new Class({
      *
      * Returns:
      * {<Jx.Tree>} a reference to this object for chaining calls
-     */
+     *
     replace: function(item, withItem) {
         item.owner = null;
         withItem.owner = this;
@@ -238,7 +239,7 @@ Jx.Tree = new Class({
     /**
      * Method: cleanup
      * Clean up a Jx.Tree instance
-     */
+     *
     cleanup: function() {
         // stop updates when removing existing items.
         this.freeze();
@@ -270,7 +271,7 @@ Jx.Tree = new Class({
      *
      * Parameters:
      * shouldDescend - {Boolean} propagate changes to child nodes?
-     */
+     *
     update: function(shouldDescend, isLast) {
         // since the memory leak cleanup, it seems that update gets called
         // from the bound onRemove event after the list has been cleaned
@@ -304,7 +305,7 @@ Jx.Tree = new Class({
      * return an array of tree item instances contained in this tree.
      * Does not descend into folders but does return a reference to the
      * folders
-     */
+     *
     items: function() {
         return this.list.items().map(function(item) {
             return item.retrieve('jxTreeItem');
@@ -313,7 +314,7 @@ Jx.Tree = new Class({
     /**
      * APIMethod: empty
      * recursively empty this tree and any folders in it
-     */
+     *
     empty: function() {
         this.list.items().each(function(item){
           var f = item.retrieve('jxTreeItem');
@@ -337,7 +338,7 @@ Jx.Tree = new Class({
      *
      * Returns:
      * {Object} the node or null if the path was not found
-     */
+     *
     findChild : function(path) {
         //path is empty - we are asking for this node
         if (path.length === 0) {
@@ -376,7 +377,7 @@ Jx.Tree = new Class({
      *
      * Returns:
      * {<Jx.Tree>} a reference to this object for chaining
-     */
+     *
     setSelection: function(selection) {
         if (this.selection && this.ownsSelection) {
             this.selection.removeEvents(this.bound);
@@ -391,4 +392,3 @@ Jx.Tree = new Class({
         return this;
     }
 });
-
